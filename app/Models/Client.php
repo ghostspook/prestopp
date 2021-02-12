a49b653854dbb8d22b96024ec62a578bfe06c993
Rafael Castillo <pcastill@mac.com>
2021-02-11 21:48:22 -0500

    Added City and Client models

---
+++ app/Models/Client.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Client extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'city_id',
+    ];
+
+    public function City() {
+        return $this->belongsTo(City::class);
+    }
+}
