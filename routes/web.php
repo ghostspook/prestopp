c54d94f3beb26abd9a3682e6d28c3df0c467fa2e
Rafael Castillo <pcastill@mac.com>
2021-02-11 23:58:16 -0500

    Redirect / to /home. Protect /home

--- routes/web.php
+++ routes/web.php
@@ -16,11 +16,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('home');
 });
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware('auth');
 
 Route::resource('clients', ClientController::class)->except('show')->middleware('auth');
