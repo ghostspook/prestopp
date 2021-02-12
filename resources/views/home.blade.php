85777e1e09f01c4ec8e28336adfc1e540c4c6253
Rafael Castillo <pcastill@mac.com>
2021-02-12 00:03:54 -0500

    Tablero resumen

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,16 +5,18 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Resumen</div>
 
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    <p>
+                        Clientes:
+                        <a href="{{ route('clients.index') }}">
+                            {{ \App\Models\Client::count() }}
+                        </a>
+                    </p>
+                    <p>
+                        Usuarios: {{ \App\Models\User::count() }}
+                    </p>
                 </div>
             </div>
         </div>
